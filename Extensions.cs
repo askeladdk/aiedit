c8ec73bc3a1590ab3a7a55c29efc4ed5312f5ea7
askeladdk <askeladdk@users.noreply.github.com>
2016-01-17 23:11:47 +0100

    class method extensions

---
+++ Extensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace AIEdit
+{
+	public static class Extensions
+	{
+		public static T Get<T>(this IOrderedDictionary dict, string key) where T:class
+		{
+			return dict[key] as T;
+		}
+
+		public static string GetString(this IOrderedDictionary dict, string key)
+		{
+			return dict[key] as string;
+		}
+
+		public static int GetInt(this IOrderedDictionary dict, string key)
+		{
+			return int.Parse(dict[key] as string);
+		}
+
+		public static uint GetUint(this IOrderedDictionary dict, string key)
+		{
+			return uint.Parse(dict[key] as string);
+		}
+
+		public static bool GetBool(this IOrderedDictionary dict, string key)
+		{
+			return GetString(dict, key).CompareTo("yes") == 0;
+		}
+
+		public static AITypeListEntry GetAITypeListEntryByIndex(this IList list, int index)
+		{
+			foreach(AITypeListEntry entry in list)
+			{
+				if (entry.Index == index) return entry;
+			}
+			return null;
+		}
+
+		public static uint SwapEndianness(this uint x)
+		{
+			return ((x & 0x000000ff) << 24) |
+				   ((x & 0x0000ff00) <<  8) |
+				   ((x & 0x00ff0000) >>  8) |
+				   ((x & 0xff000000) >> 24);
+		}
+	}
+}
