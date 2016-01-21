e38543f2d27b5631bc7cb5e22826b8826374f218
askeladdk <askeladdk@users.noreply.github.com>
2016-01-21 22:59:24 +0100

    version info

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,14 +6,14 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("AIEdit")]
+[assembly: AssemblyTitle("C&C AI Editor")]
 [assembly: AssemblyDescription("AI Editor for RA2 and TS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Askeladd")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Askeladd")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
+[assembly: AssemblyCulture("")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
